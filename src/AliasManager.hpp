8395a44d4280072cc13f63b8b6d2a596d6690a05
Andrzej Fiedukowicz <fiedukow@gmail.com>
2012-04-28 17:59:14 +0200

    AliasManager prototype added.

---
+++ src/AliasManager.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <glibmm/ustring.h>
+#include <iostream>
+#include <map>
+
+/**
+ * Class used to two things:
+ * 1. Translate alias->ip ip->alias
+ * 2. Colecting informations about subscription
+ *    especially state of subscription request and
+ *    provide those information for view
+ */
+class AliasMaganer
+{    
+};
