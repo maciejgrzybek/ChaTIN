5260f667573541becac50fc5f4cc980a0ed58555
Andrzej Fiedukowicz <fiedukow@gmail.com>
2012-04-29 20:22:49 +0200

    Constructor to cast between IPv6 and Alias added. Casting from IPv6 to Alias is always allowed but casting from Alias to IPv6 is valid if Alias string is valid ipv6 adress which can be validate by Socket::isValidIP.

--- src/types.hpp
+++ src/types.hpp
@@ -4,29 +4,41 @@ namespace ChaTIN
 /**
  * Class created to gain types controll from language
  * It is string but it can be used only in some contexts
- */
-struct IPv6 : public Glib::ustring
-{};
-
-/**
- * Class created to gain types controll from language
- * It is string but it can be used only in some contexts
  * It can be "real" alias or IPv6 if user does not have alias
  *  but even if it has IPv6 it has to be translated via AliasManager
  *  to IPv6 type.
  */
-struct Alias : public Glib::ustring
-{};
+class Alias : public Glib::ustring
+{
+public:
+    /**
+     * Copy from IPv6 adress its valid alias anyway
+     */
+    Alias( const IPv6& );
+};
+
+/**
+ * Class created to gain types controll from language
+ * It is string but it can be used only in some contexts
+ */
+class IPv6 : public Glib::ustring
+{
+    /**
+     * Just cast to IPv6 if Alias is IP adress
+     * @throw CannotCastToIPv6Exception when alias isnt valid IPv6 adress
+     */
+    IPv6( const Alias& ); //FIXME - throw declaration
+};
 
 /**
  * Class created to gain types controll from language
  * It holds name of the conference and ip of its owner
  *  which is enough data to identify conference by ConferenceManager
  */
-struct ConferenceId : public Glib::ustring
+struct ConferenceId
 {
     IPv6          ownerip;
     Glib::ustring name;
 };
 
-}
+}/*namespace ChaTIN*/
