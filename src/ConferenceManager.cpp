0963b9c1f6327b3d004c4cb57d5747c6a0027634
drah3n <kumpels@gmail.com>
2012-04-29 22:43:42 +0200

    Usage [] operator than insert in ConferenceManager.cpp Add comparation members vector in registerIncomingConference

--- src/ConferenceManager.cpp
+++ src/ConferenceManager.cpp
@@ -3,53 +3,62 @@
 void ConferenceManager::addConference( 
         const ConferenceId& id, const std::vector< Glib::ustring >& members )
 {
-	std::map::iterator iter = conferences.find( id ); 
+    std::map::iterator iter = conferences.find( id ); 
     if( iter!=conferences.end() )
     {
-        throw NameAlreadyInUse( name );
-	}
+        // FIXME
+        // throw NameAlreadyInUse( name );
+    }
     else
     {
-        conferences.insert( std::pair< ConferenceId, std::vector< Glib::ustring > >( id, members); 
-	}
+        conferences[ id ] = members; 
+    }
 }
 
 const std::vector<Glib::ustring>& ConferenceManager::getList( const ConferenceId& id )
 {
-	std::map::iterator iter = conferences.find( id ); 
+    std::map::iterator iter = conferences.find( id ); 
     if( iter!=conferences.end() )
     {
         return iter->second; 
     }
     else
     {
-        throw ConferenceNotFoundException;
+        // FIXME
+        // throw ConferenceNotFoundException;
     }
 }
 
  void ConferenceManager::removeConfrence( const ConferenceId& id )
  {
-	std::map::iterator iter = conferences.find( id );
+    std::map::iterator iter = conferences.find( id );
     if( iter!=conferences.end() )
     {
         conferences.erase( iter ); 
     }
     else
     {
-        throw ConferenceNotFoundException;
+        // FIXME
+        // throw ConferenceNotFoundException;
     }
 }
 
 void ConferenceManager::registerIncomingConference( 
             const ConferenceId& id, const std::vector< Glib::ustring >& members )
 {
-	std::map::iterator iter = conferences.find( id ); 
+    std::map::iterator iter = conferences.find( id ); 
     if( iter!=conferences.end() )
     {
-		//FIXME
-	}
+        if ( std::equal ( iter->second.begin(), iter->second.end(), members ) ) //FIXME test it
+        {
+        }
+        else
+        {
+            //FIXME unknown behavior
+        }
+    }
     else
     {
-        conferences.insert( std::pair< ConferenceId, std::vector< Glib::ustring > >( id, members); 
-	}
+        conferences[ id ] = members;  
+    }
 }
