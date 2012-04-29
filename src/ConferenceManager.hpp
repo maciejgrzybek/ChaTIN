b683a08ddd3a047c01126fb8d9189cbeb21c030b
Andrzej Fiedukowicz <fiedukow@gmail.com>
2012-04-29 17:02:42 +0200

    Some logical changes in ConferenceManager. Now conference is identifyed by name & owner ipv6 and not only name with often '~1' issue.

--- src/ConferenceManager.hpp
+++ src/ConferenceManager.hpp
@@ -4,32 +4,26 @@
 
 
 /**
- * NOTE: Can we hold aliases? Isnt that bad when we call registerIncomingConfeorence in some case
- *       Should names has other namespace then aliases? 
- *        Or we only create lock(alias) unlock(alias) method in  aliasManager?
- */
-
-/**
  * Class being able to create / remove conference container holding name of conference 
  * with list of its members.
  */
 class ConferenceManager
 {
-    std::map< Glib::ustring /*name*/, std::vector< Glib::ustring /*members*/> conferences;
+    std::map< std::pair<Glib::ustring /*name*/, Glib::ustring /*owner ipv6*/>, std::vector< Glib::ustring /*members*/> conferences;
 
     /**
-     * Method that allows to create confrence but only if name isnt already registred alias
-     *  or other conference name.
-     * @throw AliasDoesNotExistsException when you try to add conference with unexisting alias
-     * @throw AliasAlreadyInUse if name was already used as alias or other conference name
+     * Method that allows to create confrence but only if name isnt already registred name 
+     *  or other conference name and owner ::1.
+     * @throw NameDoesNotExistsException when you try to add conference with unexisting name
+     * @throw NameAlreadyInUse if name was already usede
      */
     void addConference( const Glib::ustring& name, const std::vector< Glib::ustring >& members );
 
     /**
-     * Method that can be used to find list of aliases in conference name of which we know
+     * Method that can be used to find list of ipv6 in conference given by name
      * @param cosnt Glib::ustring& name of conference
      * @throw ConferenceNotFoundException when there is no conference with given name
-     * @return reference to list of aliases in conference given by name in argument
+     * @return reference to list of ipv6 in conference given by name in argument
      */
     const std::vector<Glib::ustring>& getList( const Glib::ustring& name );
 
@@ -41,16 +35,14 @@ class ConferenceManager
     void removeConfrence( const Glib::ustring& name );
 
     /**
-     * Find if incoming name is already in use
+     * Find if incoming name+ipv6 pair is already in use
      * If it isnt just do addConferece
      * otherwise - create new name for that and try again
-     * If you find conference with same name (on one can be generated from it)
-     *  it compares vectors of members to find out if its the same conference
-     *  if it is does nothing, other wise finds good name for new one and addit
-     * note: it can be easliy implemented using recursion
-     * members to find out if its the same conference.
+     * If you find conference with same name+ipv6 pair
+     *  compare vectors of members to find out if its the same conference
+     *  if it is do nothing, otherwise find new name (~1,~2,~3...) for new one and add it
      */
     void registerIncomingConference( 
-            const Glib::ustring& name, const std::vector< Glib::ustring >& members );
+            const Glib::ustring& name, const Glib::ustring& owner, const std::vector< Glib::ustring >& members );
     
 };
