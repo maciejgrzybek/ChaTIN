b8544522af154f2bbe02316a9e606e8a62227dfc
Andrzej Fiedukowicz <fiedukow@gmail.com>
2012-05-04 21:28:16 +0200

    Main file creates all main objets without threads Well this is the start of very long way. Interfece need some fixes now

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include "ChatWindow.hpp"
+#include "DBDriver.hpp"
+#include "ToViewParser.hpp"
+#include "SafeQueue.hpp"
+#include "Config.hpp"
+#include "types.hpp"
+#include "ConferenceManager.hpp"
+#include "AliasManager.hpp"
+#include "DialogManager.hpp"
+#include "FromViewParser.hpp"
 
 
 int main(int argc, char *argv[])
 {
     Gtk::Main kit(argc, argv);
+    DBDriver db;
+    SafeQueue<ChaTIN::IncomingMassage> toViewParserQueue;
+    ToViewParser toViewParser(toViewParserQueue);
+    Config config;
+    ConferenceManager conferenceManager;
+    AliasManager aliasManager( db );
+    DialogManager dialogManager( toViewParser, aliasManager, conferenceManager, config);
+    aliasManager.setDialogManager( dialogManager );
+    FromViewParser fromViewParser( dialogManager );
+    
     ChatWindow win;
     Gtk::Main::run(win);
     return 0;
