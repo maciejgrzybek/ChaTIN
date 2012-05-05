a43eb7613fba0a86ffbc9f7aa1c0e30847c4e36e
Andrzej Fiedukowicz <fiedukow@gmail.com>
2012-05-05 13:58:37 +0200

    Makefile now comiles ChatTabFactory. Small chanages in ChatTabFactory class.

--- src/ChatTabFactory.cpp
+++ src/ChatTabFactory.cpp
@@ -1,17 +1,16 @@
-#pragma once
 #include "ChatTabFactory.hpp"
 
-static std::shared_ptr<ChatTabDialog> ChatTabFactory::create( const ChaTIN::Alias& alias )
+std::shared_ptr<ChatTabDialog> ChatTabFactory::create( const ChaTIN::Alias& alias )
 {
     return std::shared_ptr<ChatTabDialog>(new ChatTabDialog(alias));
 }
 
-static std::shared_ptr<ChatTabConference> ChatTabFactory::create( const ChaTIN::ConferenceId& name )
+std::shared_ptr<ChatTabConference> ChatTabFactory::create( const ChaTIN::ConferenceId& name )
 {
     return std::shared_ptr<ChatTabConference>(new ChatTabConference(name));
 }
 
-static std::shared_ptr<ChatTabLog> ChatTabFactory::create( const Glib::ustring& name )
+std::shared_ptr<ChatTabLog> ChatTabFactory::create( const Glib::ustring& name )
 {
     return std::shared_ptr<ChatTabLog>(new ChatTabLog(name));
 }
