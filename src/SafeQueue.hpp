0de192648c2749b989fdb9f9e320fcbd90421e46
Maciej 'mac' Grzybek <mashester@gmail.com>
2012-05-16 18:32:10 +0200

    SafeQueue reimplementation by justsoftwaresolutions.co.uk

--- src/SafeQueue.hpp
+++ src/SafeQueue.hpp
@@ -2,73 +2,52 @@
 #include <queue>
 #include <boost/thread/mutex.hpp>
 #include <boost/interprocess/sync/scoped_lock.hpp>
-#include <boost/interprocess/sync/interprocess_semaphore.hpp>
+#include <boost/thread/condition_variable.hpp>
+
 
 /**
-* Its just std::queue with mutex on every operation
-* MBO Whole class ;-)
-*/
-template < class T, class Container = std::deque<T> >
-class SafeQueue : protected std::queue<T,Container>
+ * Source from: www.justsoftwaresolutions.co.uk
+ */
+template<typename Data>
+class SafeQueue
 {
-    typedef std::queue<T,Container> baseQueue;
-    boost::mutex m;
-    /* used to avoid active wait for something in queue */
-    boost::interprocess::interprocess_semaphore sizeSem;
-
+private:
+    std::queue<Data> the_queue;
+    mutable boost::mutex the_mutex;
+    boost::condition_variable the_condition_variable;
 public:
-    SafeQueue() : sizeSem(0)
-    {}
+    void push(Data const& data)
+    {
+        boost::mutex::scoped_lock lock(the_mutex);
+        the_queue.push(data);
+        lock.unlock();
+        the_condition_variable.notify_one();
+    }
 
     bool empty() const
     {
-        boost::interprocess::scoped_lock<boost::mutex>(m);
-        return baseQueue::empty();
-    }
-    size_t size() const
-    {
-        boost::interprocess::scoped_lock<boost::mutex>(m);
-        return baseQueue::size();
-    }
-    T& front ( )
-    {
-        sizeSem.wait(); //must be in this order
-        boost::interprocess::scoped_lock<boost::mutex>(m);
-        sizeSem.post();
-        return baseQueue::front();
-    }
-    const T& front ( ) const
-    {
-        boost::interprocess::scoped_lock<boost::mutex>(m);
-        return baseQueue::front();
-    }
-    T& back ( )
-    {
-        sizeSem.wait(); //must be in this order
-        boost::interprocess::scoped_lock<boost::mutex>(m);
-        sizeSem.post();
-        return baseQueue::back();
-    }
-    const T& back ( ) const
-    {
-        boost::interprocess::scoped_lock<boost::mutex>(m);
-        return baseQueue::back();
-    }
-    void push ( const T& x )
-    {
-        boost::interprocess::scoped_lock<boost::mutex>(m);
-        sizeSem.post();
-        return baseQueue::push(x);
+        boost::mutex::scoped_lock lock(the_mutex);
+        return the_queue.empty();
     }
 
-    /**
-     * This method will hang process if queue is empty
-     */
-    void pop ( )
+    Data front()
     {
-        sizeSem.wait(); //must be in this order
-        boost::interprocess::scoped_lock<boost::mutex>(m);
-        return baseQueue::pop();
+        boost::mutex::scoped_lock lock(the_mutex);
+        while(the_queue.empty())
+        {
+            the_condition_variable.wait(lock);
+        }
+        return the_queue.front();
+    }
+
+    void pop()
+    {
+        boost::mutex::scoped_lock lock(the_mutex);
+        while(the_queue.empty())
+        {
+            the_condition_variable.wait(lock);
+        }
+        
+        the_queue.pop();
     }
 };
-
