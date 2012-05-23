353c605f42b3893bb961adde5343be4b70e90ced
Andrzej Fiedukowicz <fiedukow@gmail.com>
2012-05-23 11:45:40 +0200

    ConferenceManager::registerIncomingConference look fixed

--- src/ConferenceManager.cpp
+++ src/ConferenceManager.cpp
@@ -45,44 +45,7 @@ void ConferenceManager::registerIncomingConference(
             const ChaTIN::ConferenceId& id, const std::vector< ChaTIN::IPv6 >& members )
 {
     conferenceMap::const_iterator iter = conferences.find( id );
-    if( iter!=conferences.end() )
-    {
-    /*    //FIXME test it
-    *    if ( std::equal ( iter->second.begin(), iter->second.end(), members ) )
-    *    {
-    *    }
-    *    else
-    *    {
-    *        Glib::ustring tempName;
-    *        tempName = id->first.name;
-    *        ChaTIN::ConferenceId tempId;
-    *        tempId.ownerip = id->first.ownerip;
-    *        int maxConference = 256;
-    *        for (int i = 1; i <= maxConference ; i++)
-    *        {
-    *            std::stringstream nameNumber;
-    *            nameNumber tempName << "~" << i;
-    *            Glib::ustring newName = nameNumber.str();
-    *            tempId.name = newName;
-    *            iter = conferences.find( tempId );
-    *            if( iter!=conferences.end() )
-    *            {
-    *               if ( std::equal ( iter->second.begin(), iter->second.end(), members ) )
-    *               {
-    *                    return;
-    *               }
-    *               else
-    *               {
-    *                    conferences[tempId] = members;
-    *               }
-    *            }
-    *        }
-    *        trow MaxConferenceReachedException();
-    *    }
-    */
-    }
-    else
-    {
-        conferences[id] = members;
-    }
+    if( iter!=conferences.end() ) //TODO maybe it should throw exception if list of members isnt same
+        return;
+    conferences[id] = members;
 }
