bda3f7a0e2df562c9de818810dca650fe86af5ae
Maciej 'mac' Grzybek <mashester@gmail.com>
2012-06-02 14:16:24 +0200

    loading and saving subscriptions and aliases within transactions.

--- src/AliasManager.cpp
+++ src/AliasManager.cpp
@@ -200,6 +200,7 @@ void AliasManager::loadSubscriptionsFromDB()
 {
     // get aliases
     DB::Aliases aliases;
+    db.startTransaction();
     aliases = db.getAliases();
     // TODO shouldn't bimap be erased before insertions?
     DB::Aliases::const_iterator endIter = aliases.end();
@@ -208,9 +209,11 @@ void AliasManager::loadSubscriptionsFromDB()
         typedef BiStringMap::value_type pos;
         dictionary.insert(pos((*iter)->getAlias(),(*iter)->getIP()));
     }
+    db.endTransaction();
 
     // get subscriptions
     DB::Subscriptions subs;
+    db.startTransaction();
     subs = db.getSubscriptions();
     DB::Subscriptions::const_iterator endIt = subs.end();
     for(DB::Subscriptions::const_iterator iter = subs.begin();
@@ -218,10 +221,12 @@ void AliasManager::loadSubscriptionsFromDB()
     {
         subscriptions[(*iter)->getIP()] = static_cast<SubPhase>((*iter)->getState());
     }
+    db.endTransaction();
 }
 
 void AliasManager::saveSubscriptionsToDB()
 {
+    db.startTransaction();
     // save aliases
     BiStringMap::const_iterator endIter = dictionary.end();
     for(BiStringMap::const_iterator iter = dictionary.begin(); iter != endIter; ++iter)
@@ -229,7 +234,9 @@ void AliasManager::saveSubscriptionsToDB()
         DB::Schema::Alias alias(iter->left,iter->right);
         db.store(alias);
     }
+    db.endTransaction();
 
+    db.startTransaction();
     // save subscriptions
     std::map<ChaTIN::IPv6, SubPhase>::const_iterator endIt = subscriptions.end();
     for(std::map<ChaTIN::IPv6, SubPhase>::const_iterator iter = subscriptions.begin();
@@ -238,4 +245,5 @@ void AliasManager::saveSubscriptionsToDB()
         DB::Schema::Subscription sub(static_cast<std::string>(iter->first),iter->second);
         db.store(sub);
     }
+    db.endTransaction();
 }
